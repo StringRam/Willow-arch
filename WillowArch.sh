f1a211c4ed499d394baea8184c46a8f7a0e2f1b7
Mateo Correa Franco <27649122+StringRam@users.noreply.github.com>
2025-07-09 17:13:02 -0300

    Final sed commands fix

--- WillowArch.sh
+++ WillowArch.sh
@@ -559,10 +559,10 @@ Exec = /usr/bin/rsync -a --delete /boot /.bootbackup
 EOF
 
 info_print "Enabling colours, animations, and parallel downloads for pacman."
-sed -Ei 's/^#(Color)$/\1\nILoveCandy/;s/^#(ParallelDownloads).*/\1 = 10/' /mnt/etc/pacman.conf
+sed -i -e 's/^#Color$/Color/' -e 's/^#ILoveCandy$/ILoveCandy/' -e 's/^ParallelDownloads.*/ParallelDownloads = 10/' /mnt/etc/pacman.conf
 
 info_print "Enabling multilib repository in pacman.conf."
-sed -i '/^\[multilib\]/,/^\[/{s/^#//}' /mnt/etc/pacman.conf
+sed -i '/^#\[multilib\]/,/^$/{s/^#//}' /mnt/etc/pacman.conf
 
 if [[ $aur_bool -eq 1 ]]; then
     install_aur_helper
