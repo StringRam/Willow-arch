dcd55691946eea5049b27958dc176b5902c8991b
Mateo Correa Franco <27649122+StringRam@users.noreply.github.com>
2025-07-10 00:01:04 -0300

    Virtualization check

--- WillowArch.sh
+++ WillowArch.sh
@@ -38,7 +38,7 @@ error_print () {
 
 
 #┌──────────────────────────────  ──────────────────────────────┐
-#                          Initial checks
+#                              Checks
 #└──────────────────────────────  ──────────────────────────────┘
 check_uefi() {
     if [[ -f /sys/firmware/efi/fw_platform_size ]]; then
@@ -76,6 +76,32 @@ check_clock_sync() {
             exit 1
         fi
     fi
+}
+
+# Virtualization check (function).
+virt_check () {
+    hypervisor=$(systemd-detect-virt)
+    case $hypervisor in
+        kvm )   info_print "KVM has been detected, setting up guest tools."
+                pacstrap /mnt qemu-guest-agent &>/dev/null
+                systemctl enable qemu-guest-agent --root=/mnt &>/dev/null
+                ;;
+        vmware  )   info_print "VMWare Workstation/ESXi has been detected, setting up guest tools."
+                    pacstrap /mnt open-vm-tools >/dev/null
+                    systemctl enable vmtoolsd --root=/mnt &>/dev/null
+                    systemctl enable vmware-vmblock-fuse --root=/mnt &>/dev/null
+                    ;;
+        oracle )    info_print "VirtualBox has been detected, setting up guest tools."
+                    pacstrap /mnt virtualbox-guest-utils &>/dev/null
+                    systemctl enable vboxservice --root=/mnt &>/dev/null
+                    ;;
+        microsoft ) info_print "Hyper-V has been detected, setting up guest tools."
+                    pacstrap /mnt hyperv &>/dev/null
+                    systemctl enable hv_fcopy_daemon --root=/mnt &>/dev/null
+                    systemctl enable hv_kvp_daemon --root=/mnt &>/dev/null
+                    systemctl enable hv_vss_daemon --root=/mnt &>/dev/null
+                    ;;
+    esac
 }
 
 
@@ -482,6 +508,9 @@ fstab_file
 sed -i "/^#$locale/s/^#//" /mnt/etc/locale.gen
 echo "LANG=$locale" > /mnt/etc/locale.conf
 echo "KEYMAP=$kblayout" > /mnt/etc/vconsole.conf
+
+# Virtualization check.
+virt_check
 
 info_print "Configuring /etc/mkinitcpio.conf."
 cat > /mnt/etc/mkinitcpio.conf <<EOF
