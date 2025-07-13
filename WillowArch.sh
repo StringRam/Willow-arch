94689483416b57573d4b5d989c2a8575d32e862a
Mateo Correa Franco <27649122+StringRam@users.noreply.github.com>
2025-07-13 00:17:44 -0300

    Update WillowArch.sh
    Deleted eye candy option from pacman.conf

--- WillowArch.sh
+++ WillowArch.sh
@@ -536,8 +536,8 @@ When = PostTransaction
 Exec = /usr/bin/rsync -a --delete /boot /.bootbackup
 EOF
 
-info_print "Enabling colours, animations, and parallel downloads for pacman."
-sed -Ei 's/^#(Color)$/\1\nILoveCandy/;s/^#(ParallelDownloads).*/\1 = 10/' /mnt/etc/pacman.conf
+info_print "Enabling colours and parallel downloads for pacman."
+sed -Ei 's/^#(Color)$/\1/;s/^#(ParallelDownloads).*/\1 = 10/' /mnt/etc/pacman.conf
 
 info_print "Enabling multilib repository in pacman.conf."
 sed -i "/^#\[multilib\]/,/^$/{s/^#//}" /mnt/etc/pacman.conf
